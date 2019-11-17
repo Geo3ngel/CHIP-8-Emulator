6436bc04be74deebc7a3e5c36d54c6a285445993
GeoEngel <GeoEngel.z@gmail.com>
2019-11-16 20:15:36 -0700

    Added screen's base variables

--- src/Screen.cs
+++ src/Screen.cs
@@ -1,7 +1,11 @@
-﻿namespace CHIP-8-Emulator
+﻿namespace src
 {
     public class Screen
     {
-        
+        // Initalizes Screen Size
+        int screen_wdith = 64, screen_height = 32;
+
+        // Array of pixels (Defined as booleans as Chip-8 only uses 2 colors)
+        Action<bool[,]> pixels;
     }
 }
