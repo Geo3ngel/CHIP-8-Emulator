43f9cf52125f13e7b581f78769670cf6ea9f79c5
GeoEngel <GeoEngel.z@gmail.com>
2019-11-16 20:16:04 -0700

    Added basic global variables to the Chip-8 class

--- src/Chip8.cs
+++ src/Chip8.cs
@@ -1,7 +1,39 @@
-﻿namespace CHIP-8-Emulator
+﻿using System;
+
+namespace src
 {
     public class Chip8
     {
-        
+        //Initialization for Chip-8 VM Components
+
+        byte[] registers = new byte[16];
+
+        //Timers
+        byte delay;
+        byte sound;
+
+        Action<int> beep;
+
+        ushort stack_pointer = new ushort[16];
+
+        byte[] ram = new byte[0x1000];
+
+        //Opcodes
+        Dictionary<byte, Action<OpCodeData>> opCodes;
+        Dictionary<byte, Action<OpCodeData>> opCodesMisc; // TODO: Merge these together?
+
+        HashSet<byte> keys_pressed = new HashSet<byte>();
+
+        Random random = new Random();
+
+        public Chip8(Action<bool[,]> pixels, Action<int> beep)
+        {
+        TODO: // Set up graphics here
+            this.beep = beep;
+        }
+
+
+
+
     }
 }
