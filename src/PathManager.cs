b2d961b5d13f4931ad0d7c3ba81a6bde2c0b794d
GeoEngel <GeoEngel.z@gmail.com>
2019-11-20 00:44:28 -0700

    Added some relative pathing to the games folder in PathManager

--- src/PathManager.cs
+++ src/PathManager.cs
@@ -6,11 +6,42 @@ namespace CHIP_8_Emulator
     public class PathManager
     {
         private string root_project_path;
+        private string games_path;
 
         public PathManager()
         {
-            root_project_path = "";
-            Console.WriteLine("ROOT: {0}", root_project_path);
+            // Initializes the root path to the project directory.
+            root_project_path = "..\\..\\";
+            Console.WriteLine("ROOT: {0}", Path.GetFullPath(root_project_path));
+
+            games_path = init_game_path();
+            Console.WriteLine("Games dir: {0}", Path.GetFullPath(games_path));
+
+            get_games()
+            //Path.Combine(Outputter_dir, filename);
+
+            while (true)
+            {
+
+            }
+        }
+
+        private string init_game_path()
+        {
+            // TODO: Verify this path is still valid, if not, create a "games" directory
+            return Path.Combine(root_project_path, "games");
+        }
+
+        private string[] get_games()
+        {
+            string[] games = Directory.GetFiles(games_path);
+
+            foreach (string game_filename in games)
+            {
+                Console.WriteLine("Game: {0}", game_filename);
+            }
+
+            return games;
         }
 
     }
